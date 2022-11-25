9917abb143ef172d8cddcbab8b2956f3828bd8af
DmitriyB763 <mr.diy3292@gmail.com>
2022-11-25 19:05:57 +0400

    first commit

---
+++ src/main/java/ru/netology/domaine/data/DataClass.java
@@ -0,0 +1,41 @@
+package ru.netology.domaine.data;
+
+public class DataClass {
+    private String name;  //поле содержащие имя фильма
+    private String genre; //поле содержащие жанр фильма
+    private int id; // поле содержащие id  фильма
+
+    public DataClass() {
+    }
+
+    public DataClass(String name, String genre, int id) {
+        this.name = name;
+        this.genre = genre;
+        this.id = id;
+    }
+
+    //сформировал getter`ы и setter`ы на поля
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
