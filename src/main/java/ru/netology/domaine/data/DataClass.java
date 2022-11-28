0434339959e1b20a918aed0c327f14d0aea918d9
DmitriyB763 <mr.diy3292@gmail.com>
2022-11-28 20:39:10 +0400

    added metods

--- src/main/java/ru/netology/domaine/data/DataClass.java
+++ src/main/java/ru/netology/domaine/data/DataClass.java
@@ -38,4 +38,6 @@ public class DataClass {
     public void setId(int id) {
         this.id = id;
     }
+
+
 }
--- src/main/java/ru/netology/domaine/manager/MovieManager.java
+++ src/main/java/ru/netology/domaine/manager/MovieManager.java
@@ -2,43 +2,52 @@ package ru.netology.domaine.manager;
 
 import ru.netology.domaine.data.DataClass;
 
+import java.util.Arrays;
+
 public class MovieManager {
 
-    public static void main(String[] args) {
-//создал экземпляры класса
-        DataClass first = new DataClass ("вперед","комедия",1);
-        DataClass second = new DataClass("бладшот","боевик",2);
-        DataClass third = new DataClass("отель белград","боевик",3);
-        DataClass four = new DataClass("джентельмены","боевик",4);
-        DataClass five = new DataClass("тролли","комедия",5);
-        DataClass six = new DataClass("человек-невидимка","ужасы",6);
-        DataClass seven = new DataClass("трансформеры","боевик",7);
-        DataClass eight = new DataClass("мстители","боевик",8);
-        DataClass nine = new DataClass("желеный человек","боевик",9);
-        DataClass ten = new DataClass("ночь в музее","комедия",10);
+  private  int countMovies = 10;
 
-        //создал массив для хранения фильмов
-        DataClass[] movie = new DataClass[0];
-//добавить фильм
-        public void addMovie (DataClass movie) {
+    public MovieManager() {
+    }
+
+    public MovieManager(int countMovies) {
+        this.countMovies = countMovies;
+    }
+
+    //создал массив для хранения фильмов, изначально пустой
+   private DataClass[] moviesList = new DataClass[0];
+
+    //добавить фильм
+    public void addMovie(DataClass movie) {
 //создаю новый массив на 1 больше
-            int length = movie.length+1;
+        DataClass[] tmp = Arrays.copyOf(moviesList, moviesList.length + 1); // метод копирования из старого массива в новый
+            /*int length = moviesList.length+1;
             DataClass[] tmp = new DataClass[length];
             //копирую из старого массива в новый
-            System.arraycopy(movie,0,tmp,0,movie.length);
-            //кладу в новый массив новый фильм
-            int lastIndex = tmp.length - 1;
-            tmp[lastIndex] = movie;
-            movie = tmp;
+            System.arraycopy(movie,0,tmp,0,moviesList.length);*/
+        //кладу в новый массив новый фильм
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        moviesList = tmp;
+    }
+    public DataClass[] findAll(){
+        return moviesList;
+    }
 
-
-
-
+    public DataClass[] findLast () {
+        int length = countMovies; // задаю размер лимита
+        if (moviesList.length < countMovies) { //проверяю что размер массива меньше лимита
+            length = moviesList.length; // в переменную lenth  запиши новый размер массива
         }
-
-
-
-
+        DataClass[] tmp = new DataClass[length]; // создаю новый массив (времянный)
+        for (int i = 0; i < tmp.length;i++){  //
+            tmp[i] = moviesList[moviesList.length -i - 1]; // в   ячейку i массива  tmp кладу из основного массива коследнюю ячейку -i (i  согласно интерации цикла)
+        }
+        return tmp; // возвращаю массив где фильмы в обратном порядке благодаря строкам 43-45
 
     }
+
+
 }
+
